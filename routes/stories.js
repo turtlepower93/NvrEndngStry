682201cc342287e0ab1a3fd5555ce7f52c1ce6c6
Richard Williams <richelliswilliams@gmail.com>
2020-12-02 14:59:52 -0800

    route to new book page

--- routes/stories.js
+++ routes/stories.js
@@ -4,7 +4,7 @@ var storiesCtrl = require('../controllers/stories.js')
 const isLogginIn = require('../config/auth')
 
 router.get('/', storiesCtrl.index);
-// router.get('/new', isLogginIn, storiesCtrl.new);
+router.get('/new', storiesCtrl.new);
 // router.get('/:id', storiesCtrl.show);
 // router.get('/', isLogginIn, storiesCtrl.create);
 
