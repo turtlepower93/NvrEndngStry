6013da119b03027378a38840c47c12ae0c0f7841
Richard Williams <richelliswilliams@gmail.com>
2020-12-05 16:03:24 -0800

    finish delete and update

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,7 +1,50 @@
+const { render } = require('ejs');
+const { NotExtended } = require('http-errors');
+const story = require('../models/story');
 const Story = require('../models/story')
 
 module.exports = {
     create,
+    delete:deleteComment,
+    update,
+    edit
+}
+
+function edit(req,res) {
+    Story.findOne({'comments._id' : req.params.id})
+    .then(function(story) {
+        comments = story.comments.id(req.params.id)
+        return res.render(`stories/edit`, {title:'Edit Comment', story, comments})
+})}
+
+
+function update(req, res, next) {
+    console.log(req.body.content);
+    Story.findOne({'comments._id' : req.params.id})
+    .then(function(story) {
+        comments = story.comments.id(req.params.id)
+        comments.content = req.body.content;
+        story.save()
+        console.log(story)
+        res.redirect(`/stories/${story._id}`)
+    })
+}
+
+function deleteComment(req, res, next) {
+    console.log(req.params.id)
+    Story.findOne({'comments._id' : req.params.id})
+    .then(function(story) {
+        console.log(story);
+        const comment = story.comments.id(req.params.id);
+        if(!comment.user.equals(req.user._id)) return res.redirect(`/stories/${story._id}`)
+        comment.remove();
+        story.save()
+        .then(function() {
+            res.redirect(`/stories/${story._id}`)
+        }).catch(function(err) {
+            return next(err);
+        })
+    })
 }
 
 function create(req, res) {
--- routes/comments.js
+++ routes/comments.js
@@ -3,7 +3,9 @@ var router = express.Router();
 const commentsCtrl = require('../controllers/comments');
 const isLoggedIn = require('../config/auth')
 
-router.post('/stories/:id/comments', isLoggedIn,commentsCtrl.create)
-//router.delete('/stories/:id', isLoggedIn, commentsCtrl.delete)
+router.post('/stories/:id/comments', isLoggedIn, commentsCtrl.create)
+router.delete('/comments/:id', isLoggedIn, commentsCtrl.delete)
+router.get('/:id/edit', isLoggedIn, commentsCtrl.edit);
+router.put('/comments/:id', isLoggedIn, commentsCtrl.update)
 
 module.exports = router;
